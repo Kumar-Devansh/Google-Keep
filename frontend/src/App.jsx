70b0024aa4372eb5485263a02d6ebcc2c12d364b
Kumar-Devansh <devanshup1312@gmail.com>
2026-06-22 15:35:05 +0530

    Fixed Errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
@@ -49,6 +49,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route path='*' element={<Navigate to='/' replace />} />
           </Routes>
         </BrowserRouter>
       </NoteProvider>
